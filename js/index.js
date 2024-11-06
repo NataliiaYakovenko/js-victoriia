2651000e06e0e3aa11c0b7ab162b51e2cdc34f83
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2024-11-06 23:19:27 +0200

    Add let and const

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 alert('Hello,world!');
+console.log('Hi)))');
+
+let userName = 'text';
+console.log(userName);
+userName = 1;
+console.log(userName);
+
+const userDay = 10;
+console.log(userDay);
+
+var userAnother = 'Rol';
+console.log(userAnother);
+
