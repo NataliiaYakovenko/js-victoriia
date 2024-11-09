0543ed9c863c95ceea3466bf0621b4fd505967d0
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2024-11-09 22:24:27 +0200

    Add task

--- js/index.js
+++ js/index.js
@@ -1,23 +1,6 @@
-// Визначення типу даних typeof
-console.log('typeof 10:', typeof 10)
+//Задача
+const myName = 'Nataliia';
+console.log('Nataliia', typeof myName);
 
-const objValue = {};
-console.log('typeof objValue:', typeof objValue);
-
-let value;
-console.log('typeof value:', typeof value);
-
-const bigintValue = 11n;
-console.log(typeof bigintValue);
-
-const numberA = '11n';
-console.log(typeof numberA);
-
-const isOdd = true;
-console.log(typeof isOdd);
-
-const isOdd1 = 'true';
-console.log(typeof isOdd1);
-
-const null1 = null;
-console.log(typeof null1);
+const myOldd = 41;
+console.log('41', typeof myOldd);
