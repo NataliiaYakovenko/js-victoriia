75b30949e0fbe3e9ff1d87e2fd0554057e87e229
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2024-11-10 08:40:21 +0200

    Add operations for number

--- js/index.js
+++ js/index.js
@@ -1,6 +1,44 @@
-//Задача
-const myName = 'Nataliia';
-console.log('Nataliia', typeof myName);
+//Операції:
+//операції для чисел
+let namber1 = 10; //перше присвоєння називається ініціалізацією
+namber1 = 0; //присвоєння
 
-const myOldd = 41;
-console.log('41', typeof myOldd);
+//арифметичні
+let arithmetic1 = 1 + 2;
+console.log(arithmetic1);
+
+let arithmetic2 = 10 - 2;
+console.log(arithmetic2);
+
+let arithmetic3 = 2 * 2;
+console.log(arithmetic3);
+
+let arithmetic4 = 7 / 2;
+console.log(arithmetic4);
+
+//залишок від ділення
+let arithmetic5 = 7 % 2;
+console.log(arithmetic5); // = 1
+
+//піднесення до степеня
+let arithmetic6 = 6**3; 
+console.log(arithmetic6); // 216
+
+//Math.(через консольку) - для інших нестандартних обчислень
+let arithmetic7 = (Math.sqrt(3)); 
+console.log(arithmetic7); 
+
+let arithmetic8 = (Math.pow(4, 4)); 
+console.log(arithmetic8); 
+
+//Знайти довжину кола за радіусом: 2 * PI *r
+let r = 10;
+let arithmetic9 = 2 * Math.PI *r; 
+console.log(arithmetic9); 
+
+//Одночасно дкілька обчислень
+let arithmetic10 = 2*2+3-5 ; 
+console.log(arithmetic10); 
+
+let arithmetic11 = 2**3**2 ; 
+console.log(arithmetic11); 
