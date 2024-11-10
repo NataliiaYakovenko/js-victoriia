e0ff0091c53567fde43bb6c358b324b2650cc81d
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2024-11-10 15:29:18 +0200

    Task

--- js/index.js
+++ js/index.js
@@ -1,22 +1,7 @@
-//Приведення типів
-//Вести два числа 5 і 100, і вивести їх суму
-const number1 = prompt('Введіть перше число:');//наприклад 5
-// const number2 = prompt('Введіть друге число:');наприклад 100
-const number2 = 100; //або
-const sum = number1 + number2;
+//task
+//Вивести три чесла і порахувати їх добуток
+const number1 = Number (prompt('First number:'));
+const number2 = Number (prompt('Secong number:'));
+const number3 = Number(prompt('Third number:'));
+const sum = number1 * number2 * number3;
 console.log(sum);
-//результат = 5100 (нам не підходить)
-
-//Вирішення задачи тип рядка замінити на число
-const number3 = Number(prompt('Введіть перше число:'));//наприклад 5
-// const number2 = prompt('Введіть друге число:');наприклад 100
-const number4= Number ('100'); //або
-const sum1 = number3 + number4;
-console.log(sum1);
-
-//Тип числа замінити на рядок
-const years1 = String ('10');
-const years2 = String ('10');
-const years = years1 + years2;
-console.log(years);
-
