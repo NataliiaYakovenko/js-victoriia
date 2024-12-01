dd6575f1989a6126515715539be04bcb76dfb96f
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2024-12-01 10:52:33 +0200

    Add task while

--- js/loops.js
+++ js/loops.js
@@ -1,84 +1,45 @@
-// while, do...while
-/*console.log('Пройти прямо');
-console.log('Повернути ліворуч');
-console.log('Пройти прямо');
-console.log('Повернути ліворуч');
-console.log('Пройти прямо');
-console.log('Повернути ліворуч');
-console.log('Пройти прямо');
-console.log('Повернути ліворуч');
-*/
+// задача
+// Порахувати суму чисел від 1 до 20
+// 1 2 3 4 5 6 7 8 9 ...
 
-/*let sideCont = 1;
-const totalSideAmount = 4;
+//Рішення
+ let i = 1; // лічильник
+ let sum = 0; //коробочка куди ми сумуємо числа
 
-while(sideCont <= totalSideAmount){
-    console.log('Пройти прямо');
-    console.log('Повернути ліворуч');
-    sideCont++ ;//sideCont += 1; //sideCont = sideCont + 1; (змінили сторону кута на 2) 
-}*/
-//sideCont - це змінна циклу
-// змінна циклу повинна закінчуватися, щоб досягти закнчення циклу
+ while(i <= 20)
+  {
+    sum = sum + i;
+    i++;
+    console.log(i);
+ }
+ console.log(sum);
 
-//Задача
-/*
-У користувача є книга з pageCount сторінок
-Вивести номери сторінок з 1 по останню
-*/
-
-/*let currentPage = 1; // поточна сторінка
-const totalPage = 10; // загальна кількість сторінок
-
-while(currentPage <= totalPage)// поточна сторінка меньше або доріанює загальної кількості сторінок
-{
-   console.log(currentPage); 
-   currentPage++; // обовєязково додавати наступну сторінку, бо цикл буде нескінченний
-
-}*/
-
-// Вивести тільки парні номери сторінок
-/*let currentPage = 1; // поточна сторінка
-const totalPage = 10; // загальна кількість сторінок
-
-while(currentPage <= totalPage)// поточна сторінка меньше або доріанює загальної кількості сторінок
-{
-    if(currentPage % 2 === 0){
-        console.log(currentPage);} 
-        
-        currentPage++;
-}*/
-
-//Ввести число і вивести його в квадрат
-//Перевірте щоб число було скінченим (не NaN, Infinity)
-//Алгоритм
-// Ввести число
-// Якщо число скінченне, вивести його в квадрат
-//Якщо число нескінченне, перейти на початок ввести число
-
-/*let n = Number (prompt('введіть число'));
-while(!Number.isFinite(n))// якщо число нескінченне
-{
-    n = Number (prompt('введіть число')); // тоді число потрібно змінювати
-}
-console.log(n*n);
-*/
-
-// do...wihle
- /*let n = null;
- do {
-    n = Number(prompt('введіть число'))
- }while(!Number.isFinite(n))
-    console.log(n*n);*/
- 
-    
-//Вивеcти номери сторінок за допомогою do...while
-let currentPage = 0; // поточна сторінка
-const totalPage = 10; // загальна кількість сторінок
-
-do{
-    currentPage++;
-    console.log(currentPage); // виводжу номер поточної сторінки
-} while(currentPage<totalPage);
+ // Роз'яснення
+ // i = 1 , 1 <= 20 - true
+ // sum = 0 + 1 = 1
+ // i++;// i = i + 1, це 1 + 1 = 2
+ // i = 2 , 2 <= 20 - true
+ // sum = 1 + 2 = 3
+ //  i++;// i = i + 1, це 2 + 1 = 3
+ //...
+ //...
+ //...
+ // i = 20 , 20 <= 20 - true
+ // sum = 190 + 20 = 210
+ // i++;// i = 20 + 1, це 20 + 1 = 21
+  // i = 21 , 21 <= 20 - fals
 
 
+
+  // задача 2
+// Порахувати добуток чисел від 1 до 10
+ let x = 1; // лічильник
+ let mult = 1; //коробочка куди ми помножуємо числа
+
+ while(x <= 10){
+    mult = mult * x;
+    x++;
+    console.log(x);
+ }
+ console.log(mult)
 
