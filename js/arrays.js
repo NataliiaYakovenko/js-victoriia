cbf8626ffeb4e95e1ba02c4296c1e74023927485
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2024-12-21 21:08:29 +0200

    Add методи перебору масива

--- js/arrays.js
+++ js/arrays.js
@@ -1,76 +1,61 @@
-// МЕТОДИ МАСИВІВ
-
-//1 array.push - додавання ОДНОГО елементу в кінець масива
-const numbers = [10,20,30];
-numbers.push(40);
-console.log(numbers);
-
-//Ввести кількість елементів масиву і динамічно заповнити масив
-/*const elementCount = Number(prompt('Enter count:'))//Скільки разів потрібно вводити числа
-const count =[];
-
-for(let i = 0; i < elementCount;i++){
-    const element = Number(prompt('Enter number:'))//вводити числа
-    count.push(element)
+//Методи перебору масивів
+//Callback - це функція, яка передається до іншої функції і виконується в ній
+function functionWithCallback(cd,value){
+    cd(value)
 }
-console.log(count)*/
-//-------------------------------------------------------------------------
+//functionWithCallback(alert,'Hello')
+functionWithCallback(console.log,'Hello from console');
+//-------------------------------------------------------------------------------
 
-//2 array.pop - видалення останього елементу з масиву
-const numbers1 = [10,20,30];
-const deletedItem = numbers1.pop()
-console.log(deletedItem)// виведе видалений елемент 30
-console.log(numbers1);// виведе масив [10,20]
-//------------------------------------------------------------------------
+//array.forEach - використовується для перебору (ітерації) кожного елемента масиву і виконання певної дії для виконання кожного елемента
+//array.forEach(callback) - викликає функцію зворотнього виклику
+//Callback - це функція, яка передається до іншої функції і виконується в ній
+//Застосування Callback 
+const array1 = [1,2,3,4,5,6,]
 
-//3 array.shift - видалення першого елементу з масиву
-const numbers2 = [10,20,30];
-numbers2.shift()
-console.log(numbers2);
-//-------------------------------------------------------------------------
+function printItem(item, index, array){
+console.log(`Item ${index} = ${item}`);
+console.log(array);
+}
+array1.forEach(printItem)
 
-//4 array.unshift - додавання першого елементу до масиву
-const numbers3 = [10,20,30];
-numbers3.unshift(0)
-console.log(numbers3);
-//-------------------------------------------------------------------------
-
-//array1.slice - копією весь масив або його частину,створюючи новий масив
-const numbers4 = [100,200,300,400];
-const numbersCopy = numbers4.slice()// зкопіює весь масив
-const numbersCopy1 = numbers4.slice(2)// копіювання елементів з першого індексу
-const numbersCopy2 = numbers4.slice(1,3)//копіювання елементів з першого індексу по другий
-//-------------------------------------------------------------------------
-
-//array.splice - видаляє, додає елементи масива
-const numbers5 = [100,200,300,400];
-numbers5.splice(2,1)//з другого індексу видалити 1 елемент (300)
-console.log(numbers5);
-//---------------------------------------------------------------------------
-
-//Задача видалити перший елемент
-//Скопіювати два останні елементи
-const numbers6 = [100,200,300,400];
-numbers6.splice(0,1)
-console.log(numbers6)
-//Скопіювати два останні елементи
-const numbers6Copy = numbers6.slice(1)
-console.log(numbers6Copy);
-//або//Скопіювати два останні елементи
-const numbers7 = [100,200,300,400,500,600,700];
-let numbers7Copy = numbers7.slice(numbers7.length-2)//останні два елементи, незалежно скільки їх додасться в майбутньому
-console.log(numbers7Copy);
+//зробити теж саме через цикл for
+for(let i = 0;i < array1.length;i++){
+    printItem(array1[i], i, array1)
+}
 //----------------------------------------------------------------------------
 
-//array.сoncat - з'єднує один масив з іншим  
-// варіант1
-const numbers8 = [10,20,30,40];
-const numbers9 = [100,200,300,400];
-const numbers10 = numbers8.concat(numbers9)
-console.log(numbers10)
+//array.map - працює як forEach (викликає функцію зворотнього виклику), 
+//тільки map буде повертати(створювати) новий масив  (немутуючий)
 
-// варіант2
-const numbers11 = [10,20,30,40];
-const numbers12 = [100,200,300,400];
-const numbers13 = [...numbers11,...numbers12]
-console.log(numbers13);
+//Згенерувати масив в квадраті
+const array2 = [1,2,3,4,5,6,]
+
+function toSquer(item){
+return item**2;
+}
+
+const newArray2 = array2.map(toSquer)
+console.log(newArray2)
+
+
+//Отимати елементи з вихідного, змінивши знаки елементами
+const array3 = [1,2,3,4,5,6,]
+
+function changingSignsElements(item){
+return -item;//змінивши знаки -1, -2, -3, -4, -5, -6
+}
+const newArray3 = array3.map(changingSignsElements)
+console.log(newArray3)
+
+
+//Отримати масив з іменами користувачів
+const users =[
+    {id: 1, name:'Test'},
+    {id: 2, name:'Ivo'}
+  ];
+  function getName(user){
+    return user.name;
+  }
+const name = users.map(getName)
+console.log(name);
