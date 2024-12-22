2a349eb7159cf1ad4d8ddd77575c397aa6907169
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2024-12-22 20:24:34 +0200

    Add arrow function

--- js/arrowFunction.js
+++ js/arrowFunction.js
@@ -1,52 +1,90 @@
-// rest - параметри(залишкові параметри)
-//rest - параметри- потрібно оголошувати
-function f(a,b,...rest) {
-    console.log(rest)//потрапить все крім a,b,  - потрапить [3, 4, 5]
+function name(params) {
+  // Function declarstion
 }
-const result = f(1,2,3,4,5)
-//------------------------------------------------------------------------------------------------
-//                           Порівняння arguments i rest-parameters
-//
-//                   | властивості функції | треба оголошувати|   тип    |   всі параметри?
-//   arguments       |             +       |          -       | Arguments|        +
-//   rest-parameters |             -       |          +       |   Array  |        -
-//                   |                     |                  |          |
-//-------------------------------------------------------------------------------------------------
+//---------------------------------------------------
+const func = function name() {
+  // Function expression
+};
+//--------------------------------------------------
+const fun = () => {}; // Arrow function
+// Arrow function - немає arguments
+// Arrow function - немає this, бере з середовища, не використовується як метод об'кта
 
-//Порахувати суму елементів
-//array.reduce - використовується для обчислення якогось ОДНОГО значення на основі всіх елементів масиву (не мутуючий)
-//array.reduce - зводить всі елементи масиву до якогось одного результуючого значення
-//array.reduce(calback, initialValue) - синтакс
-function summa(...summands) {
-//в rest - параметрі створюємо calback
-    function howToReduce(acumulative,item){// acumulative - накопичення результату,item - поточний елемент(перший)
-      return  acumulative = acumulative + item;//накопичення результату + поточний елемент
-    }
- const sum = summands.reduce(howToReduce)//використовується для обчислення якогось ОДНОГО значення на основі всіх елементів
- return sum;
-}
-const result1 = summa(1,2,3,4,5,6)
-console.log(result1);
-//-------------------------------------------------------------------------------------------------
-// Задача rest - parameters
-function colculation(operation,...operands) {
-    let result;
-    if (operation === "+") {
-      result = 0;
-      for (let i = 0; i < operands.length; i++) {
-        result += operands[i];
-      }
-    }else if(operation === "*"){
-      result = 1;
-      for (let i = 0; i < operands.length; i++) {
-          result *= operands[i];
-        }
-    }else{
-      return undefined;
-    }
-    return result;
+//приклад
+const sum = (a, b) => {
+  return a + b;
+};
+console.log(sum(7, 8));
+
+//Скорочена форма
+const sum1 = (a, b) => a + b;
+console.log(sum1(4, 8));
+
+//-------------------------------------------------
+//приклад
+//Написати функцію для визначення чи повнолітня людина
+//Приймає вік
+//Повертає true для повнолітнього, false для неповнолітнього
+const isAdult = (age) => {
+  return age >= 18;
+};
+console.log(isAdult(20));
+console.log(isAdult(12));
+
+//Скорочений варіант
+const isAdult1 = (age) => age >= 18;
+
+console.log(isAdult1(2));
+console.log(isAdult1(18));
+//-------------------------------------------------------
+//приклад. в функцію запакувати об'єкт
+const pacToObjact = (firstName, lastName) => {
+  return {
+    firstNam: "Nataliia",
+    lastName: "Yakovenko",
+  };
+};
+console.log(pacToObjact());
+
+//короткий запис
+const pacToObjact1 = (firstName, lastName) => ({
+ //прибираємо return і об'єкт перемо у (душки)   
+    firstNam: "Nataliia",   
+    lastName: "Yakovenko",
+  });
+console.log(pacToObjact1());
+console.dir(pacToObjact1());
+//-----------------------------------------------------
+
+//приклад
+/*написати функцію привітати корисувача
+якщо вибрати  en: то повернути Hello + ім'я користувача; 
+якщо вибрати  ua: то повернути Вітаю + ім'я користувача; 
+якщо вибрати  fran: то повернути Bomgure + ім'я користувача; 
+ */
+const greetingUserName = (lang,userName)=>{
+  if(lang === 'en'){
+    return `Hello, ${userName}`
+  } else if(lang === 'ua'){
+    return `Вітаю, ${userName}`
+  } else if(lang === 'fran'){
+     return `Bongure, ${userName}`
   }
-  //arguments                         0, 1, 2, 3, 4, 5, 6
-  const summaOperands = colculation("*", 2, 2, 2, 2, 2, 2);
-  //rest - parameters                    0, 1, 2, 3, 4, 5
-  console.log(summaOperands);
+}
+console.log(greetingUserName('en', 'Alex'));
+console.log(greetingUserName('ua', 'Nataliia'));
+console.log(greetingUserName('fran', 'Franzua'));
+
+
+//варіант2, зробити через об'єкт
+const greeting = {
+  en:'Hello',           //властивість об'єкта
+  ua:'Вітаю',
+  fran:'Bongurno'
+}
+const greetingUserName1 = (lang,userName)=>`${greeting[lang]} ${userName}`
+//greeting[lang] - властивість об'єкта
+
+console.log(greetingUserName('en', 'Bob'));
+console.log(greetingUserName('ua', 'Taras'));
+console.log(greetingUserName('fran', 'Jozefina'));
