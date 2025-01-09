37cba20a6167c8d5dadd91166103a79438f04b79
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-01-09 21:35:47 +0200

    Add Symbol ітераторні колекції

--- js/symbol.js
+++ js/symbol.js
@@ -35,3 +35,71 @@ switch (themes) {
   default:
     console.log("unexpacted value");
 }
+//---------------------------------------------------------
+
+//Symbol.iterator
+/*
+1. Таку колекцію можна перебирати через for...of
+
+2. Для колекції можна використовувати деструктуризацію
+const [first,second] =[1,2]
++ для об'єктів як виняток
+
+3.Spread оператор [...]
+створення копії колекції
+конкатенація колекції
+const array = [1,2,3,4,5]
+const arrayCopy=[...array]
++ для об'єктів як виняток
+*/
+//Приклад
+function sum(){
+console.log(arguments);
+const arrayFromArguments =[...arguments]   //перетворити arguments на array
+console.log(arrayFromArguments);
+return arrayFromArguments.reduce((colculation,item)=>colculation+item)
+}
+console.log(sum(1,2,3,4,5));
+//передача елементів масиву в функцію
+const array1 =[1,2,3,4,5,6,7]
+console.log(sum(...array1));
+
+//Злиття об'ктів разом
+const user1 ={
+    firstName: 'Nataliia',
+    lastName:'Yakovenko',
+}
+const user2 ={
+    isMail: true,
+    age: 20,
+}
+//беремо все що було, а далі все, що змінюємо
+let mainUser ={...user1,...user2}
+console.log(mainUser);
+const newName = 'Natasha'
+mainUser ={...mainUser,firstName:newName}
+console.log(mainUser);
+//----------------------------------------------------
+//Приклад 2
+const contactUsars =[
+    {id:1, name:'Olga',age: 31, tel:'+3809998877'},
+    {id:2, name:'Boris',age: 35, tel:'+3809998866'},
+    {id:3, name:'Vova',age: 34, tel:'+3809998855'},
+    {id:4, name:'Tera',age: 25, tel:'+3809998844'},
+]
+person =  {id:2, name:'Boris',age: 35, tel:'+3803333333'}
+const foundIndex =contactUsars.findIndex(u=>u.id===person.id)
+console.log(foundIndex);
+console.log(contactUsars[foundIndex]=person);
+console.log(contactUsars[foundIndex]={...contactUsars[foundIndex],...person});
+//---------------------------------------------------------------
+//Приклад 3
+//Отримання набору опцій з користувацьких і дефолтних
+function sendDate(date,option){
+ const defaultOptions = {host:'localhost', port: 5400}
+ const resultOptions ={...option,...defaultOptions}
+ console.log(resultOptions);
+}
+sendDate({id:2, name:'Boris',age: 35, tel:'+3803333333', port:5005})
+
+//4 Array.from() - створює масив з псевдомасива фбо ітерованої колекції
