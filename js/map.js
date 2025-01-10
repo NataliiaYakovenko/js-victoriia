f58e54f136bfbb48f90138e19e8c388730249c70
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-01-10 11:20:58 +0200

    Add практика map словник - перекладач

--- js/map.js
+++ js/map.js
@@ -1,57 +1,32 @@
-/*Map
-Колекція по ключу, яка може як ключ містити будь-який тип
-*/
 
-const map1 = new Map();
-console.log(map1);
-console.log(map1.size);
+//----------------------------------------------------------------------
+//Практика map
+//Перекладач. Словник міститься в мапі
+ //1 отримати масив англомовних слів ['dog', 'run', 'white', 'cat']
+  //2 по славнику, якщо є відповідний ключ, тоді отримати для ключів значення 
+      //['собака', 'бігає', 'white', 'котом']
+      //а якщо немає тоді залишити як є
+  //3 з масива отримати рядок
 
-map1.set("firstname", "test");
-console.log(map1);
+const vocabulary = new Map()
+vocabulary.set('dog','собака').set('cat','kit').set('run','бігати')
+console.log(vocabulary);
 
-//задаємо об'кт
-map1.set("firstname", "Roland").set("lastname", "Symonian");
-map1.set({ id: 8, name: "Nataliia" }, ["Hello", "I am fine"]);
-const copyMap1 = { id: 8, name: "Nataliia" };
+function translateEngToUa(engPhrase){
+  //1 отримати масив англомовних слів ['dog', 'run', 'white', 'cat']
+  const engWords = engPhrase.split(' ')
+  console.log(engWords);
+  //2 по славнику, якщо є відповідний ключ, тоді отримати для ключів значення 
+      //['собака', 'бігає', 'white', 'котом']
+      //а якщо немає тоді залишити як є
 
-//отримувати
-console.log(map1.get("firstname"));
-console.log(copyMap1);
-//---------------------------------------------------------
-//has
-//obj in key
-console.log(map1.has("lastname"));
-console.log(map1.has("age"));
+  const uaWords = engWords.map(word=>vocabulary.has(word)?vocabulary.get(word):word)    
+  console.log(uaWords);
+  //3 з масива отримати рядок
+const uaPhrase = uaWords.join(' ')
+  return uaPhrase;
+}
 
-const m1 = new Map();
-
-m1.set("Olga", "+3809998877")
-  .set("Boris", "+3809998866")
-  .set("Vova", "+3809998855")
-  .set("Tera", "+3809998844");
-console.log(m1.has("Vova"));
-console.log(m1.has("Jon"));
-
-//delete obj key
-m1.delete('Boris')
-console.log(m1);
-
-//clear - почистити map
-//m1.clear()
-//console.log(m1);
-
-
-//forEach
-m1.forEach((value,key)=>{console.log(key,value)})
-
-console.log(m1.keys());
-
-const mapKeys = [...m1.keys()]
-console.log(mapKeys);
-console.log(...m1.values());
-
-//створити мапу з об'єкта
-const obj ={tell1: '+380998765544',tell2:'+380887654433'}
-//отримуємо масив з об'єкту
-console.log(Object.entries(obj));
-const m2 = new Map(Object.entries(obj))
+const engPhrase = 'dog run white cat '
+const ukrPhrase =  translateEngToUa(engPhrase)
+console.log(ukrPhrase);
